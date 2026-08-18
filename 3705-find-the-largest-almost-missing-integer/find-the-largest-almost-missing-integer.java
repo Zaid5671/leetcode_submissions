e0af829f717a84ff0f4f0be2106e3624e013de91
Zaid Ahmed Khan <zaidahmedkhan567@gmail.com>
2026-08-18 06:42:11 +0530

    Time: 1 ms (99.30%) | Memory: 44.7 MB (74.74%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,40 +1,40 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        int[] freq = new int[51];
         int n = nums.length;
+        int[] freq = new int[51];
+        
+        // Build the frequency map once for all cases
+        for (int num : nums) {
+            freq[num]++;
+        }
+        
         int max = -1;
-        if(k==1){
-            for(int i = 0;i<n;i++){
-                freq[nums[i]]++;
+        
+        if (k == n) {
+            // If k == n, we just want the maximum element present in the array
+            for (int i = 0; i <= 50; i++) {
+                if (freq[i] > 0) {
+                    max = i; 
+                }
             }
-            for(int i = 0;i<51;i++){
-                if(freq[i]==1){
+        } else if (k == 1) {
+            // If k == 1, we want the maximum element that appears exactly once
+            for (int i = 0; i <= 50; i++) {
+                if (freq[i] == 1) {
                     max = i;
                 }
             }
-        }else if(k==n){
-            for(int i : nums){
-                max = Math.max(max,i);
+        } else {
+            // If 1 < k < n, only the first and last elements can be in exactly 1 subarray.
+            // They are valid only if their total frequency in the array is exactly 1.
+            if (freq[nums[0]] == 1) {
+                max = nums[0];
+            }
+            if (freq[nums[n - 1]] == 1) {
+                max = Math.max(max, nums[n - 1]);
             }
         }
-        else{
-            int l = nums[0];
-            int r = nums[n-1]; 
-            if(l==r){
-                l = -1;
-                r = -1;
-            }
-            for(int i = 1;i<n-1;i++){
-                if(nums[i]==l){
-                    l = -1;
-                }
-                if(nums[i]==r){
-                    r = -1;
-                }
-            }
-            max = Math.max(max,Math.max(l,r));
-        }
-
+        
         return max;
     }
 }

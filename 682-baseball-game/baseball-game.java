87cdc3ee9ff5f6f005dce6c4f4fa65881b131b45
Zaid Ahmed Khan <zaidahmedkhan567@gmail.com>
2026-09-16 18:44:47 +0530

    Time: 3 ms (79.52%) | Memory: 43.1 MB (97.74%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int calPoints(String[] a) {
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for(int i = 0;i<a.length;i++){
+            String curr = a[i];
+            if(curr.equals("D")){
+                st.push(st.peek()*2);   
+            }
+            else if(curr.equals("+")){
+                int num1 = st.pop();
+                int num3 = num1+st.peek();
+                st.push(num1);
+                st.push(num3);
+            }else if(curr.equals("C")){
+                st.pop();
+            }else{
+                st.push(Integer.parseInt(curr));
+                
+            }
+        }
+        int sum = 0;
+        while(!st.isEmpty()){
+            sum+= st.pop();
+        }
+
+        return sum;
+    }
+}

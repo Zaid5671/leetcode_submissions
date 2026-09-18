123e8bbee5f365593117fbe063263043c14bc404
Zaid Ahmed Khan <zaidahmedkhan567@gmail.com>
2026-09-18 12:01:30 +0530

    Time: 3 ms (79.27%) | Memory: 43.8 MB (14.78%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -4,26 +4,24 @@ class Solution {
 
         for(int i = 0;i<a.length;i++){
             String curr = a[i];
-            if(curr.equals("D")){
-                st.push(st.peek()*2);   
-            }
-            else if(curr.equals("+")){
+            if(curr.equals("+")){
                 int num1 = st.pop();
                 int num3 = num1+st.peek();
                 st.push(num1);
                 st.push(num3);
             }else if(curr.equals("C")){
                 st.pop();
+            }else if(curr.equals("D")){
+                st.push(st.peek()*2);
             }else{
                 st.push(Integer.parseInt(curr));
-                
             }
-        }
+        } 
+
         int sum = 0;
         while(!st.isEmpty()){
-            sum+= st.pop();
+            sum += st.pop();
         }
-
         return sum;
     }
 }

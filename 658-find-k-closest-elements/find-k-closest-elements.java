60ced213db8cea35e1fb28b9a209567927ecdc8c
Zaid Ahmed Khan <zaidahmedkhan567@gmail.com>
2026-09-23 20:17:04 +0530

    Time: 8 ms (45.84%) | Memory: 53.9 MB (6.64%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,30 +1,29 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int sumOfD = 0;
-        int prevSum = 0;
-        int startIdx = 0;
-        int n = arr.length;
         List<Integer> list = new ArrayList<>();
 
-        for(int i = 0;i<k;i++){
-            sumOfD += Math.abs(x-arr[i]);
-        }
-        
-        for(int r = k;r<n;r++){
-            prevSum = sumOfD;
-            sumOfD -= Math.abs(x-arr[r-k]);
-            sumOfD += Math.abs(x-arr[r]);
-            System.out.println(sumOfD);
-            if(sumOfD>prevSum){
-                break;
+        int l = 0;
+        int r = arr.length - k;
+        //The reason we do not compare mid to mid + k - 1 is because we are not trying to evaluate the current window itself. We are trying to decide: Should we slide this window to the right
+        //If you compare arr[mid] to arr[mid + k - 1], you are just comparing the left end of your current window to the right end of your current window.
+        //Knowing which end of your current window is closer to x does not tell you anything about what lies outside the window. 
+        //You could have a perfect match sitting at mid + k, but you would never see it because you only looked at the elements already inside your boundaries.
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            // i still didn't understood this maths trick
+            if (x - arr[mid] > arr[mid + k] - x) {
+                l = mid + 1;//cause you know for sure mid is not the ans
+            } else {
+                r = mid;// cause u can't say if mid is the ans or not
             }
-            else if(sumOfD<prevSum){
-                startIdx = r-k+1;
-            }
         }
-        // if()
+
         // create array
-        for(int i = startIdx;i<startIdx+k;i++){
+        //A while loop only breaks when `l` is equal to `r`. 
+        //Hence you could use any of them for the loop. 
+        //Also hence, you do not use `mid` but you use `L` or `R` because, from the whole of the array, you took that one index. You shrunk the space like that so that you could have that one index, which could be the perfect starting index. `mid` is something that is always changing; that is just a guess. I know that in general the answer is `mid`, but not here. That is only when you have to find something. Here you are shrinking the search space to exactly one element and that element is equal to `L = R`. 
+        System.out.println(l + "  " + r);
+        for (int i = l; i <= l + k - 1; i++) {
             list.add(arr[i]);
         }
         return list;

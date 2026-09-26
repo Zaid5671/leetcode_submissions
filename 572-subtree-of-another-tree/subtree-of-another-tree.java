74c817cbef8f8d6128b7e10b97f3ae5af2a46115
Zaid Ahmed Khan <zaidahmedkhan567@gmail.com>
2026-09-26 12:11:13 +0530

    Time: 2 ms (99.25%) | Memory: 46.3 MB (70.54%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -13,21 +13,19 @@
  *     }
  * }
  */
-
-class Solution {  
+class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null && subRoot == null)return true;
-        if(root == null || subRoot == null)return false;
         Queue<TreeNode> q = new LinkedList<>();
-
+        boolean isAns = false;  
         q.offer(root);
-
         while(!q.isEmpty()){
             TreeNode curr = q.poll();
-            if(isSame(curr,subRoot)){
+            if(curr.val == subRoot.val){
+                isAns = isEqual(curr,subRoot);
+            }
+            if(isAns){
                 return true;
             }
-
             if(curr.left != null){
                 q.offer(curr.left);
             }
@@ -35,15 +33,18 @@ class Solution {
                 q.offer(curr.right);
             }
         }
-
-        return false;
+        return isAns;
     }
-    public boolean isSame(TreeNode p, TreeNode q){
-        if(p == null && q == null)return true;
-        if(p == null || q == null)return false;
-
-        if(p.val != q.val)return false;
-
-        return isSame(p.left,q.left) && isSame(p.right,q.right); 
+    public boolean isEqual(TreeNode p, TreeNode q){
+        if(p == null && q == null){
+            return true;
+        }
+        else if(p == null || q == null){
+            return false;
+        }
+        else if(p.val != q.val){
+            return false;
+        }
+        return isEqual(p.right,q.right) && isEqual(p.left,q.left);
     }
 }

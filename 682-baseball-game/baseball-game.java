4c4b3acd78c09c632d4ed49a366b51545b7a5219
Zaid Ahmed Khan <zaidahmedkhan567@gmail.com>
2026-09-26 15:07:12 +0530

    Time: 3 ms (78.85%) | Memory: 43.3 MB (82.09%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,27 +1,28 @@
 class Solution {
-    public int calPoints(String[] a) {
+    public int calPoints(String[] o) {
         Deque<Integer> st = new ArrayDeque<>();
-
-        for(int i = 0;i<a.length;i++){
-            String curr = a[i];
+        for(int i = 0;i<o.length;i++){
+            String curr = o[i];
             if(curr.equals("+")){
                 int num1 = st.pop();
-                int num3 = num1+st.peek();
+                int num2 = st.peek()+num1;
                 st.push(num1);
-                st.push(num3);
-            }else if(curr.equals("C")){
-                st.pop();
+                st.push(num2);
             }else if(curr.equals("D")){
                 st.push(st.peek()*2);
+            }else if(curr.equals("C")){
+                st.pop();
             }else{
                 st.push(Integer.parseInt(curr));
             }
-        } 
+        }
 
+        // sum of all vals
         int sum = 0;
         while(!st.isEmpty()){
             sum += st.pop();
         }
+
         return sum;
     }
 }
